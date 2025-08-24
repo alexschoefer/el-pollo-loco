5e64f8198b56a41ac85b817a5a0b27bc7f8859b6
alexschoefer <alex.schoefer@gmx.net>
2025-08-24 17:13:37 +0200

    feat: add new function animateBottle for the bottle rotation by throwing

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,11 +1,22 @@
 class ThrowableObject extends MoveableObject {
 
+       IMAGES_BOTTLES_THROWING = [
+        'assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ]
+
     constructor(x, y) {
-        super().loadImage('assets/img/6_salsa_bottle/salsa_bottle.png');
+        super();
+        this.loadImages(this.IMAGES_BOTTLES_THROWING);
+        this.img = this.imageCache[this.IMAGES_BOTTLES_THROWING[0]];
         this.x = x;
         this.y = y;
         this.height = 80;
+        this.width = 60; 
         this.throwBottle();
+        this.animateBottle();
     }
 
     throwBottle() {
@@ -14,5 +25,11 @@ class ThrowableObject extends MoveableObject {
          setInterval(() => {
             this.x += 10;
          }, 50);
+    }
+
+    animateBottle() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_BOTTLES_THROWING);
+        }, 100);
     }
 }
