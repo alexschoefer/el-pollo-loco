d9d75884d421f26b171ea700157aa2813b0f4919
alexschoefer <alex.schoefer@gmx.net>
2025-08-31 21:44:08 +0200

    feat: animation of the clouds is working again

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,15 +1,32 @@
 class Cloud extends MoveableObject {
     y = 20;
     width = 500;
-    height = 200;
+    height = 250;
+
+    IMAGES_CLOUD = [
+        'assets/img/5_background/layers/4_clouds/1.png',
+        'assets/img/5_background/layers/4_clouds/2.png'
+    ]
 
     constructor() {
         super().loadImage('assets/img/5_background/layers/4_clouds/1.png');
-        this.x = 120 + Math.random() * 500;
+        this.setRandomCloudPosition();
         this.animateClouds();
     }
 
+    getRandomCloudImages() {
+        let randomCloudImage = Math.floor(Math.random() * 2);
+        return this.IMAGES_CLOUD[randomCloudImage];  
+    }
+
+    setRandomCloudPosition() {
+        this.x = Math.floor(Math.random() * 2000);
+    }
+
     animateClouds() {
-        this.moveLeft();
+        setInterval(() => {
+            this.moveLeft();
+        }, 60);
+
     }
 }
--- levels/level1.js
+++ levels/level1.js
@@ -5,12 +5,15 @@ const maxBottles = bottles.length;
 
 const level1 = new Level(
     [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        // new SmallChicken()
+        // new Chicken(),
+        // new Chicken(),
+        // new Chicken(),
+        // // new SmallChicken()
     ],
     [
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
         new Cloud(),
         new Cloud()
     ],
