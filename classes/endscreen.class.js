011f5e82d6455b9c2bc8dca529e24f5f66c16d04
alexschoefer <alex.schoefer@gmx.net>
2025-09-15 23:33:56 +0200

    docs: add JSDoc for these classes: endboss, endscreen, small-chicken, all statusbar classes and throwable-object

--- classes/endscreen.class.js
+++ classes/endscreen.class.js
@@ -1,7 +1,27 @@
+/**
+ * Represents the end screen displayed after the game finishes.
+ * Depending on the outcome, it can show a "You Win" or "Game Over" image.
+ * Inherits from {@link DrawableObject}.
+ */
 class Endscreen extends DrawableObject {
+    /**
+     * Path to the "Game Over" image.
+     * @type {string}
+     */
     static IMAGE_GAMEOVER = 'assets/img/You won, you lost/Game Over.png';
+
+    /**
+     * Path to the "You Win" image.
+     * @type {string}
+     */
     static IMAGE_WIN = 'assets/img/You won, you lost/You win B.png';
 
+    /**
+     * Creates an instance of the Endscreen.
+     * Automatically sets image, size, and position based on the image type.
+     * 
+     * @param {string} imagePath - The path to the image to display (win or game over).
+     */
     constructor(imagePath) {
         super();
         this.loadImage(imagePath);
@@ -15,14 +35,23 @@ class Endscreen extends DrawableObject {
             this.height = 150;
         }
 
+        // Center the endscreen on the canvas
         this.x = (720 - this.width) / 2;
         this.y = (480 - this.height) / 2;
     }
 
+    /**
+     * Makes the endscreen visible so it will be drawn.
+     */
     show() {
         this.visible = true;
     }
 
+    /**
+     * Draws the endscreen image on the canvas, if visible.
+     * 
+     * @param {CanvasRenderingContext2D} ctx - The rendering context to draw on.
+     */
     draw(ctx) {
         if (this.visible) {
             super.draw(ctx);
--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,15 +1,36 @@
+/**
+ * Class representing a throwable bottle object.
+ * Extends MoveableObject to inherit movement and physics behavior.
+ */
 class ThrowableObject extends MoveableObject {
 
+    /**
+     * Manages audio playback for the throwable object.
+     * @type {AudioManager}
+     */
     audioManager;
+
+    /**
+     * Indicates whether the bottle has splashed.
+     * @type {boolean}
+     */
     hasSplashed = false;
 
+    /**
+     * Image paths for the bottle's throwing animation frames.
+     * @type {string[]}
+     */
     IMAGES_BOTTLES_THROWING = [
         'assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         'assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
         'assets/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
         'assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
-    ]
+    ];
 
+    /**
+     * Image paths for the bottle's splash animation frames.
+     * @type {string[]}
+     */
     IMAGES_BOTTLES_SPLASH = [
         'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
         'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
@@ -17,8 +38,16 @@ class ThrowableObject extends MoveableObject {
         'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
         'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
         'assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
-    ]
+    ];
 
+    /**
+     * Creates a throwable bottle object at the specified position with an audio manager.
+     * Loads all necessary images and starts the throw and animation processes.
+     * 
+     * @param {number} x - The initial horizontal position of the bottle.
+     * @param {number} y - The initial vertical position of the bottle.
+     * @param {AudioManager} audioManager - The audio manager to handle sound effects.
+     */
     constructor(x, y, audioManager) {
         super();
         this.loadImages(this.IMAGES_BOTTLES_THROWING);
@@ -33,6 +62,10 @@ class ThrowableObject extends MoveableObject {
         this.audioManager = audioManager;
     }
 
+    /**
+     * Initiates the bottle's throwing motion by setting vertical speed and applying gravity.
+     * Moves the bottle horizontally to the right at a fixed interval.
+     */
     throwBottle() {
         this.speedY = 30;
         this.applyGravity();
@@ -41,6 +74,11 @@ class ThrowableObject extends MoveableObject {
         }, 50);
     }
 
+    /**
+     * Controls the animation of the bottle.
+     * Plays the throwing animation until the bottle splashes,
+     * then plays the splash animation and plays the throw sound once.
+     */
     animateBottle() {
         let soundPlayed = false;
 
